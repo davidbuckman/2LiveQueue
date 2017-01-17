4cdf38d6acbe0e397ffc776e4820daec1c3fd7f1
davidbuckman <davidbuckman7@gmail.com>
2017-01-16 20:22:38 -0500

    Filter words into levels by percentage cutoff list

--- models/backupqueue.js
+++ models/backupqueue.js
@@ -16,23 +16,37 @@ function shuffleArray(array) {
 
 function BackupQueue() {
     
-    var level_lists = {
-            'level1' : require('../resources/level1_wordlist.json').slice(),
-            'level2' : require('../resources/level2_wordlist.json').slice(),
-            'level3' : require('../resources/level3_wordlist.json').slice(),
-            'level4' : require('../resources/level4_wordlist.json').slice(),
-            'level5' : require('../resources/level5_wordlist.json').slice()
+    var word_list = require('../resources/wordlist.json');
+    
+    var level_cutoffs = [0, 0.5, 0.7, 0.85, 0.95, 1];
+    
+    var get_region = function(level_num) {
+        return [Math.round(word_list.length * level_cutoffs[level_num - 1]),
+                Math.round(word_list.length * level_cutoffs[level_num])];
     };
+    
+    // Words are sorted by probability of appearance, so lower probability
+    // is higher difficulty
+    var level_regions = {
+            'level5' : get_region(1),
+            'level4' : get_region(2),
+            'level3' : get_region(3),
+            'level2' : get_region(4),
+            'level1' : get_region(5)
+    };
+    
+    var level_lists = {};
 
-    for (var level in level_lists) {
-        if (!level_lists.hasOwnProperty(level)) { continue; }
+    for (var level in level_regions) {
+        if (!level_regions.hasOwnProperty(level)) { continue; }
+        level_lists[level] = word_list.slice(level_regions[level][0], level_regions[level][1]);
         shuffleArray(level_lists[level]);
     }
     
     this.get_level = function(level) {
         if (level == undefined) { level = 'level1' }
         if (level_lists[level].length == 0) {
-            level_lists[level] = require('../resources/' + level + '_wordlist.json').slice();
+            level_lists[level] = word_list.slice(level_regions[level][0], level_regions[level][1]);
             shuffleArray(level_lists[level]);
         }
         return level_lists[level].pop();
